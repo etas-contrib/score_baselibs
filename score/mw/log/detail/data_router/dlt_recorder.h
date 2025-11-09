3bd66163fe4369773022a953db1fa27ee8ad22c1
Maciej Kaszynski <kam1yok@bosch.com>
2025-11-09 22:07:04 +0000

    Some data_router stuff

---
+++ score/mw/log/detail/data_router/dlt_recorder.h
@@ -0,0 +1,65 @@
+#ifndef SCORE_MW_LOG_DLT_RECORDER_H_
+#define SCORE_MW_LOG_DLT_RECORDER_H_
+
+#include "score/mw/log/recorder.h"
+#include "score/mw/log/slot_handle.h"
+#include "score/mw/log/configuration/configuration.h"
+#include <score/optional.hpp>
+
+namespace score::mw::log::detail {
+
+class DltRecorder : public Recorder {
+    DltRecorder(const Configuration& config)
+
+  score::cpp::optional<SlotHandle>
+  StartRecord(const std::string_view context_id,
+              const LogLevel log_level) noexcept;
+
+  void StopRecord(const SlotHandle &slot) noexcept;
+
+  void Log(const SlotHandle &, const bool data) noexcept;
+
+  void Log(const SlotHandle &, const std::uint8_t) noexcept;
+  void Log(const SlotHandle &, const std::int8_t) noexcept;
+
+  void Log(const SlotHandle &, const std::uint16_t) noexcept;
+  void Log(const SlotHandle &, const std::int16_t) noexcept;
+
+  void Log(const SlotHandle &, const std::uint32_t) noexcept;
+  void Log(const SlotHandle &, const std::int32_t) noexcept;
+
+  void Log(const SlotHandle &, const std::uint64_t) noexcept;
+  void Log(const SlotHandle &, const std::int64_t) noexcept;
+
+  void Log(const SlotHandle &, const float) noexcept;
+  void Log(const SlotHandle &, const double) noexcept;
+
+  void Log(const SlotHandle &, const std::string_view) noexcept;
+
+  void Log(const SlotHandle &, const LogHex8) noexcept;
+
+  void Log(const SlotHandle &, const LogHex16) noexcept;
+
+  void Log(const SlotHandle &, const LogHex32) noexcept;
+
+  void Log(const SlotHandle &, const LogHex64) noexcept;
+
+  void Log(const SlotHandle &, const LogBin8) noexcept;
+
+  void Log(const SlotHandle &, const LogBin16) noexcept;
+
+  void Log(const SlotHandle &, const LogBin32) noexcept;
+
+  void Log(const SlotHandle &, const LogBin64) noexcept;
+
+  void Log(const SlotHandle &, const LogRawBuffer) noexcept;
+
+  void Log(const SlotHandle &, const LogSlog2Message) noexcept;
+
+  bool IsLogEnabled(const LogLevel &,
+                    const std::string_view context) const noexcept;
+};
+
+} // namespace score::mw::log::detail
+
+#endif // SCORE_MW_LOG_DLT_RECORDER_H_

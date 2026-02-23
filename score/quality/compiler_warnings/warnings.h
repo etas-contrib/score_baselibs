5c258f17ddf7db48d3970117453b7a8206d9328d
Mihajlo Katancevic <mihajlo.katancevic@bmw.de>
2026-02-23 14:39:40 +0100

    Extend compiler warning macros
    extend the list of macros to include warnings that are still manually suppressed in the codebase.
    
    GIT_ORIGIN_SPP_REV_ID: 6d2c8ebee9f820a8b33e805be2d44984d19bb561

--- score/quality/compiler_warnings/warnings.h
+++ score/quality/compiler_warnings/warnings.h
@@ -21,21 +21,32 @@
 #define DISABLE_WARNING(warningName) DO_PRAGMA(GCC diagnostic ignored #warningName)
 
 // clang-format off
-#define DISABLE_WARNING_SUGGEST_OVERRIDE              DISABLE_WARNING(-Wsuggest-override)
-#define DISABLE_WARNING_ZERO_AS_NULL_POINTER_CONSTANT DISABLE_WARNING(-Wzero-as-null-pointer-constant)
-#define DISABLE_WARNING_USELESS_CAST                  DISABLE_WARNING(-Wuseless-cast)
-#define DISABLE_WARNING_CONVERSION                    DISABLE_WARNING(-Wconversion)
-#define DISABLE_WARNING_CLASS_MEM_ACCESS              DISABLE_WARNING(-Wclass-memaccess)
-#define DISABLE_WARNING_OLD_STYLE_CAST                DISABLE_WARNING(-Wold-style-cast)
-#define DISABLE_WARNING_SHADOW                        DISABLE_WARNING(-Wshadow)
-#define DISABLE_WARNING_TYPE_LIMITS                   DISABLE_WARNING(-Wtype-limits)
+#define DISABLE_WARNING_SUGGEST_OVERRIDE                        DISABLE_WARNING(-Wsuggest-override)
+#define DISABLE_WARNING_ZERO_AS_NULL_POINTER_CONSTANT           DISABLE_WARNING(-Wzero-as-null-pointer-constant)
+#define DISABLE_WARNING_USELESS_CAST                            DISABLE_WARNING(-Wuseless-cast)
+#define DISABLE_WARNING_CONVERSION                              DISABLE_WARNING(-Wconversion)
+#define DISABLE_WARNING_CLASS_MEM_ACCESS                        DISABLE_WARNING(-Wclass-memaccess)
+#define DISABLE_WARNING_OLD_STYLE_CAST                          DISABLE_WARNING(-Wold-style-cast)
+#define DISABLE_WARNING_SHADOW                                  DISABLE_WARNING(-Wshadow)
+#define DISABLE_WARNING_TYPE_LIMITS                             DISABLE_WARNING(-Wtype-limits)
+#define DISABLE_WARNING_ARRAY_BOUNDS                            DISABLE_WARNING(-Warray-bounds)
+#define DISABLE_WARNING_UNUSED_VALUE                            DISABLE_WARNING(-Wunused-value)
+#define DISABLE_WARNING_PACKED                                  DISABLE_WARNING(-Wpacked)
+#define DISABLE_WARNING_ATTRIBUTES                              DISABLE_WARNING(-Wattributes)
+#define DISABLE_WARNING_SIGN_CONVERSION                         DISABLE_WARNING(-Wsign-conversion)
 
 #if defined(__clang__)
-#define DISABLE_WARNING_SELF_ASSIGN_OVERLOADED        DISABLE_WARNING(-Wself-assign-overloaded)
+#define DISABLE_WARNING_SELF_ASSIGN_OVERLOADED                  DISABLE_WARNING(-Wself-assign-overloaded)
+#define DISABLE_WARNING_C99_EXTENSIONS                          DISABLE_WARNING(-Wc99-extensions)
+#define DISABLE_WARNING_SELF_MOVE                               DISABLE_WARNING(-Wself-move)
+#define DISABLE_WARNING_MISSING_DESIGNATED_FIELD_INITIALIZERS   DISABLE_WARNING(-Wmissing-designated-field-initializers)
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED
 #else
 #define DISABLE_WARNING_SELF_ASSIGN_OVERLOADED
-#define DISABLE_WARNING_MAYBE_UNINITIALIZED           DISABLE_WARNING(-Wmaybe-uninitialized)
+#define DISABLE_WARNING_C99_EXTENSIONS
+#define DISABLE_WARNING_SELF_MOVE
+#define DISABLE_WARNING_MISSING_DESIGNATED_FIELD_INITIALIZERS
+#define DISABLE_WARNING_MAYBE_UNINITIALIZED                     DISABLE_WARNING(-Wmaybe-uninitialized)
 #endif
 
 // clang-format on
@@ -54,5 +65,13 @@
 #define DISABLE_WARNING_TYPE_LIMITS
 #define DISABLE_WARNING_SELF_ASSIGN_OVERLOADED
 #define DISABLE_WARNING_MAYBE_UNINITIALIZED
+#define DISABLE_WARNING_ARRAY_BOUNDS
+#define DISABLE_WARNING_SELF_MOVE
+#define DISABLE_WARNING_UNUSED_VALUE
+#define DISABLE_WARNING_PACKED
+#define DISABLE_WARNING_ATTRIBUTES
+#define DISABLE_WARNING_C99_EXTENSIONS
+#define DISABLE_WARNING_SIGN_CONVERSION
+#define DISABLE_WARNING_MISSING_DESIGNATED_FIELD_INITIALIZERS
 
 #endif
